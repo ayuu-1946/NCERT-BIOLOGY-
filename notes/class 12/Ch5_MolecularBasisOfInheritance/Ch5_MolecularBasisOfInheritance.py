10c47abd65b5d0fd5265179abd564cf56e3e731f
ayuu-1946 <316068836+ayuu-1946@users.noreply.github.com>
2026-09-26 16:44:05 +0000

    Move Figure 5.11 back one page and start Table 5.1 on page 16

--- notes/class 12/Ch5_MolecularBasisOfInheritance/Ch5_MolecularBasisOfInheritance.py
+++ notes/class 12/Ch5_MolecularBasisOfInheritance/Ch5_MolecularBasisOfInheritance.py
@@ -1185,7 +1185,8 @@ story.append(figure(
     "<b>Exon</b> and <b>Intron</b> stretches; <b>Capping</b> adds the <b>Cap</b> at the "
     "<b>5'</b> end and <b>Polyadenylation</b> adds the <b>Poly A tail</b> at the <b>3'</b> "
     "end, then <b>RNA splicing</b> removes the introns to give the mature "
-    "<b>Messenger RNA</b> -- the <b>3' mRNA</b> that is exported for translation."))
+    "<b>Messenger RNA</b> -- the <b>3' mRNA</b> that is exported for translation.",
+    max_width_cm=13.0))
 story.append(Spacer(1, 3))
 story.append(Paragraph(
     "The <b>significance of such complexities is now beginning to be understood</b>. The "
